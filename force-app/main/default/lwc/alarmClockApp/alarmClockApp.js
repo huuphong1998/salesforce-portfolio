a46e50545e19f9b8e9e1d4a0b80ddf26ea8127c7
Huu Phong <nghuuphong1998@gmail.com>
2025-09-08 16:02:09 +0700

    Adding Ringtone to an Alarm clock

--- force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -4,6 +4,7 @@ import AlarmClockAssets from "@salesforce/resourceUrl/AlarmClockAssets";
 
 export default class AlarmClockApp extends LightningElement {
     clockImage = AlarmClockAssets + "/AlarmClockAssets/clock.png";
+    ringtone = new Audio(AlarmClockAssets + "/AlarmClockAssets/Clocksound.mp3");
     currentTime = "";
     hours = [];
     minutes = [];
@@ -53,6 +54,8 @@ export default class AlarmClockApp extends LightningElement {
             if (this.alarmTime === `${hour}:${min} ${ampm}`) {
                 console.log("Alarm Trigger!");
                 this.isAlarmTriggered = true;
+                this.ringtone.play();
+                this.ringtone.loop = true;
             }
         }, 1000);
     }
@@ -96,6 +99,7 @@ export default class AlarmClockApp extends LightningElement {
         this.alarmTime = "";
         this.isAlarmSet = false;
         this.isAlarmTriggered = false;
+        this.ringtone.pause();
         const elements = this.template.querySelectorAll("c-clock-dropdown");
         Array.from(elements).forEach((element) => {
             element.reset("");
