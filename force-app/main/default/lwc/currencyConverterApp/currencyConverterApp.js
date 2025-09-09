aef565036d456469367d660a25035bc37b7c0916
Huu Phong <nghuuphong1998@gmail.com>
2025-09-09 15:04:16 +0700

    Service Component and combobox

--- force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
+++ force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import { countryCodeList } from "c/countryCodeList";
 
-export default class CurrencyConverterApp extends LightningElement {}
+export default class CurrencyConverterApp extends LightningElement {
+    countryList = countryCodeList;
+    countryFrom = "USD";
+    countryTo = "VND";
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        console.log("name", name);
+        console.log("value", value);
+        this[name] = value;
+    }
+}
