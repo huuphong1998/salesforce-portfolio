2cccf0f243be4ee4a4a193b4d978b8e39a9dd2a7
Huu Phong <nghuuphong1998@gmail.com>
2025-09-10 22:53:29 +0700

    API Integration

--- force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
+++ force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
@@ -7,11 +7,32 @@ export default class CurrencyConverterApp extends LightningElement {
     countryList = countryCodeList;
     countryFrom = "USD";
     countryTo = "VND";
+    amount = "";
+    result;
+    error;
 
     handleChange(event) {
         const { name, value } = event.target;
         console.log("name", name);
         console.log("value", value);
         this[name] = value;
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+        this.convert();
+    }
+
+    async convert() {
+        const API_URL = `https://api.exchangerate.host/convert?access_key=b6bc418327d6e052683dd80de195ca2d&from=${this.countryFrom}&to=${this.countryTo}&amount=1&format=1`;
+        try {
+            const data = await fetch(API_URL);
+            const jsonData = await data.json();
+            this.result = (Number(this.amount) * jsonData.result).toFixed(2);
+            console.log(this.result);
+        } catch (error) {
+            console.log(error);
+            this.error = "An error occurred. Please try again...";
+        }
     }
 }
