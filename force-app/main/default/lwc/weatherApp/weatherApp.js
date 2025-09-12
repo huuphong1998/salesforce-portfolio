3e1bbab620e15fcb4d1d125356d43a7adf2f97fc
Huu Phong <nghuuphong1998@gmail.com>
2025-09-12 14:41:07 +0700

    Client Side API Call

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -3,6 +3,12 @@ import { LightningElement } from "lwc";
 const API_KEY = "279c8910fc4645fec7582834d1e843a9";
 export default class WeatherApp extends LightningElement {
     cityName = "";
+    loadingText = "";
+    isError = false;
+
+    get loadingClasses() {
+        return this.isError ? "error-msg" : "success-msg";
+    }
     searchHandler(event) {
         this.cityName = event.target.value;
     }
@@ -13,6 +19,29 @@ export default class WeatherApp extends LightningElement {
     }
 
     fetchData() {
+        this.isError = false;
+        this.loadingText = "Fetching weather details...";
         console.log("cityName", this.cityName);
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&units=metric&appid=${API_KEY}`;
+        fetch(URL)
+            .then((res) => res.json())
+            .then((result) => {
+                console.log(JSON.stringify(result));
+                this.weatherDetails(result);
+            })
+            .catch((error) => {
+                console.error(error);
+                this.loadingText = "Something went wrong";
+                this.isError = true;
+            });
+    }
+
+    weatherDetails(info) {
+        if (info.cod === "404") {
+            this.isError = true;
+            this.loadingText = `${this.cityName} isn't a valid city name`;
+        } else {
+            this.loadingText = "";
+        }
     }
 }
