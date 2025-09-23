6a921528225607bb3a0e80172b7f1ccd3f9e621e
Huu Phong <nghuuphong1998@gmail.com>
2025-09-23 19:43:22 +0700

    Creating Append HTML Component

---
+++ force-app/main/default/lwc/appendHtml/appendHtml.js
@@ -0,0 +1,29 @@
+/* eslint-disable @lwc/lwc/no-inner-html */
+import { api, LightningElement } from "lwc";
+
+export default class AppendHtml extends LightningElement {
+    _result;
+    loader;
+
+    @api
+    get result() {
+        return this._result;
+    }
+    set result(data) {
+        this._result = data;
+    }
+
+    renderedCallback() {
+        if (this._result && !this.loader) {
+            this.attachHtml();
+        }
+    }
+
+    attachHtml() {
+        const container = this.template.querySelector(".htmlcontainer");
+        if (container) {
+            container.innerHTML = this.result;
+            this.loader = true;
+        }
+    }
+}
