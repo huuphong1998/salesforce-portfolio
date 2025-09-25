b195f51a3678d37c6e7f9617d35d5d0526488596
Huu Phong <nghuuphong1998@gmail.com>
2025-09-25 16:23:20 +0700

    Deleting the Note

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -2,7 +2,9 @@ import { LightningElement, wire } from "lwc";
 import createNoteRecord from "@salesforce/apex/NoteTakingController.createNoteRecord";
 import getNotes from "@salesforce/apex/NoteTakingController.getNotes";
 import updateNoteRecord from "@salesforce/apex/NoteTakingController.updateNoteRecord";
+import deleteNoteRecord from "@salesforce/apex/NoteTakingController.deleteNoteRecord";
 import { refreshApex } from "@salesforce/apex";
+import LightningConfirm from "lightning/confirm";
 
 const DEFAULT_NOTE_FORM = {
     Name: "",
@@ -129,5 +131,36 @@ export default class NoteTakingApp extends LightningElement {
 
     refresh() {
         return refreshApex(this.wiredNoteResult);
+    }
+
+    deleteNoteHandler(event) {
+        this.selectedRecordId = event.target.dataset.recordid;
+        this.handleConfirm();
+    }
+
+    async handleConfirm() {
+        const result = await LightningConfirm.open({
+            message: "Are you sure you want to delete this note?",
+            variant: "headerless",
+            label: "Delete Confirmation"
+        });
+        if (result) {
+            this.deleteHandler();
+        } else {
+            this.selectedRecordId = null;
+        }
+    }
+
+    deleteHandler() {
+        deleteNoteRecord({ noteId: this.selectedRecordId })
+            .then(() => {
+                this.showModal = false;
+                this.selectedRecordId = null;
+                this.showToastMsg("Note Deleted Successfully!!", "success");
+                this.refresh();
+            })
+            .catch((error) => {
+                console.error("error in deletion", error);
+            });
     }
 }
