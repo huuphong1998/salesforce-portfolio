d801a58a034a9431a14cb43b5ebef64d17991b0e
Huu Phong <nghuuphong1998@gmail.com>
2025-09-27 16:39:04 +0700

    Fetching Data from Salesforce to Banner Component

--- force-app/main/default/lwc/portfolioBanner/portfolioBanner.js
+++ force-app/main/default/lwc/portfolioBanner/portfolioBanner.js
@@ -1,5 +1,10 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
 import PortfolioAssets from "@salesforce/resourceUrl/PortfolioAssets";
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
+import FULLNAME from "@salesforce/schema/Portfolio__c.FullName__c";
+import COMPANY_LOCATION from "@salesforce/schema/Portfolio__c.CompanyLocation__c";
+import COMPANY_NAME from "@salesforce/schema/Portfolio__c.CompanyName__c";
+import DESIGNATION from "@salesforce/schema/Portfolio__c.Designation__c";
 
 export default class PortfolioBanner extends LightningElement {
     linkedinUrl = "https://www.linkedin.com/in/nguyenhuuphong/";
@@ -10,4 +15,28 @@ export default class PortfolioBanner extends LightningElement {
     linkedin = `${PortfolioAssets}/PortfolioAssets/Social/linkedin.svg`;
     github = `${PortfolioAssets}/PortfolioAssets/Social/github.svg`;
     trailhead = `${PortfolioAssets}/PortfolioAssets/Social/trailhead1.svg`;
+
+    recordId = "a01gL00000PNJjVQAX";
+    @wire(getRecord, { recordId: "$recordId", fields: [FULLNAME, COMPANY_LOCATION, COMPANY_NAME, DESIGNATION] })
+    portfolioData;
+    // portfolioHandler({ data, error }) {
+    //     if (data) {
+    //         console.log("record data", JSON.stringify(data));
+    //     }
+    //     if (error) {
+    //         console.log("error", error);
+    //     }
+    // }
+    get fullName() {
+        return getFieldValue(this.portfolioData.data, FULLNAME);
+    }
+    get companyLocation() {
+        return getFieldValue(this.portfolioData.data, COMPANY_LOCATION);
+    }
+    get companyName() {
+        return getFieldValue(this.portfolioData.data, COMPANY_NAME);
+    }
+    get designation() {
+        return getFieldValue(this.portfolioData.data, DESIGNATION);
+    }
 }
