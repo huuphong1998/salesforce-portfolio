4f060f953f0f42f5dcfa3c7e5f2f916ab03add8a
Huu Phong <nghuuphong1998@gmail.com>
2025-09-30 14:27:20 +0700

    Download Resume

--- force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -4,5 +4,10 @@ export default class PortfolioUserDetails extends LightningElement {
     @api recordId;
     @api objectApiName;
 
-    downloadResume() {}
+    downloadResume() {
+        window.open(
+            "https://github.com/huuphong1998/phong-resume/raw/main/CV%20-%20Nguye%CC%82%CC%83n%20Hu%CC%9B%CC%83u%20Pho%CC%82ng.pdf",
+            "_blank"
+        );
+    }
 }
