2ec01011b0735cb835ac474095dd80a9cac91c62
Huu Phong <nghuuphong1998@gmail.com>
2025-10-02 16:19:08 +0700

    Creating Summary Field and Component

--- force-app/main/default/lwc/portfolioTabsWrapper/portfolioTabsWrapper.js
+++ force-app/main/default/lwc/portfolioTabsWrapper/portfolioTabsWrapper.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class PortfolioTabsWrapper extends LightningElement {}
+export default class PortfolioTabsWrapper extends LightningElement {
+    @api recordId = "";
+    @api objectApiName = "";
+}
