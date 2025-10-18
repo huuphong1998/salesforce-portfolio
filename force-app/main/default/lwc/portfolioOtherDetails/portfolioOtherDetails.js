22c39f5a5a7386ae8373945c349f25e9aa3ac861
Huu Phong <nghuuphong1998@gmail.com>
2025-10-18 11:36:59 +0700

    Displaying information of Awards, Superbadges and languages

--- force-app/main/default/lwc/portfolioOtherDetails/portfolioOtherDetails.js
+++ force-app/main/default/lwc/portfolioOtherDetails/portfolioOtherDetails.js
@@ -3,8 +3,15 @@ import { getRecord } from "lightning/uiRecordApi";
 import SUPERBADGE_FIELD from "@salesforce/schema/Portfolio__c.Superbadges__c";
 import AWARDS_FIELD from "@salesforce/schema/Portfolio__c.Awards__c";
 import LANGUAGES_FIELD from "@salesforce/schema/Portfolio__c.Languages__c";
+import PortfolioAssets from "@salesforce/resourceUrl/PortfolioAssets";
 
 export default class PortfolioOtherDetails extends LightningElement {
+    awards = [];
+    languages = [];
+    superbadges = [];
+    awardIcon = `${PortfolioAssets}/PortfolioAssets/trophy.png`;
+    languageIcon = `${PortfolioAssets}/PortfolioAssets/language.png`;
+    badgeIcon = `${PortfolioAssets}/PortfolioAssets/badge.png`;
     @api recordId;
     @wire(getRecord, {
         recordId: "$recordId",
@@ -13,9 +20,16 @@ export default class PortfolioOtherDetails extends LightningElement {
     otherDetailsHandler({ data, error }) {
         if (data) {
             console.log("otherDetailsHandler data", JSON.stringify(data));
+            this.formatData(data);
         }
         if (error) {
             console.error("otherDetailsHandler error", error);
         }
+    }
+    formatData(data) {
+        const { Awards__c, Superbadges__c, Languages__c } = data.fields;
+        this.awards = Awards__c && Awards__c.value ? Awards__c.value.split(",") : [];
+        this.languages = Languages__c && Languages__c.value ? Languages__c.value.split(",") : [];
+        this.superbadges = Superbadges__c && Superbadges__c.value ? Superbadges__c.value.split(";") : [];
     }
 }
